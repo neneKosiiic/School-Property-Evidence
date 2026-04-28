279d065f43774725c8a5f849936f1efb23bb5a2a
Matyáš Krejcárek <KrejcarekMatyas@sssvt.cz>
2026-04-28 19:29:49 +0100

    project born

---
+++ SchoolPropertyEvidence/SchoolPropertyEvidence/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SchoolPropertyEvidence.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
