573d16297ccfcbfe2a678d0cb7c8a8df35149ecb
Matyáš Krejcárek <KrejcarekMatyas@sssvt.cz>
2026-04-28 20:45:18 +0100

    dbcontext added

---
+++ SchoolPropertyEvidence/SchoolPropertyEvidence/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolPropertyEvidence.Models;
+
+namespace SchoolPropertyEvidence.Data {
+    public class ApplicationDbContext : DbContext {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<PeopleModel> Users { get; set; } = null!;
+    }
+}
