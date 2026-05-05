c689ab0ef7db01fb1d304d9b8d534c9081da8a14
Matyáš Krejcárek <KrejcarekMatyas@sssvt.cz>
2026-05-05 17:39:28 +0100

    create other models, update people model

---
+++ SchoolPropertyEvidence/SchoolPropertyEvidence/Models/RoomModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SchoolPropertyEvidence.Models {
+
+    [Table("rooms")]
+    public class RoomModel {
+
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("room_name")]
+        public string RoomName { get; set; } = null!;
+
+        [Required]
+        [Column("responsible_person_id")]
+        public int ResponsiblePersonId { get; set; }
+
+        [ForeignKey(nameof(ResponsiblePersonId))]
+        public PeopleModel ResponsiblePerson { get; set; } = null!;
+
+        public ICollection<ItemModel> Items { get; set; } = new List<ItemModel>();
+    }
+}
